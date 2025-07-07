f087b2df352fc241daa31ea1f3fb3af655c01e0e
SnehaManoharan1 <snehamanoharan98@gmail.com>
2025-07-07 13:18:25 +0530

    Added Customer login function in FE and BE

--- frontend/src/app/CustomerPortal/customer-login/customer-login.component.ts
+++ frontend/src/app/CustomerPortal/customer-login/customer-login.component.ts
@@ -1,9 +1,15 @@
 import { Component,Inject, PLATFORM_ID , Renderer2 , ViewEncapsulation} from '@angular/core';
-import { isPlatformBrowser, NgIf } from '@angular/common';
+//import { isPlatformBrowser, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common'; 
+import { Router } from '@angular/router';
+import { CustomerLoginService } from '../../services/customer-login.service';
+import { ToastrService } from 'ngx-toastr';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-customer-login',
-  imports: [NgIf],
+  imports: [FormsModule,CommonModule],
   templateUrl: './customer-login.component.html',
   styleUrl: './customer-login.component.scss',
    encapsulation: ViewEncapsulation.None
@@ -13,11 +19,53 @@ export class CustomerLoginComponent {
 
 
     passwordVisible: boolean = false;
-  constructor(@Inject(PLATFORM_ID) private platformId: Object,  private renderer: Renderer2) {}
+    passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*\(\)+\\;:'",.<>\/?=_\{\}\[\]\|\-]).{8,}$/;
+    loginDetails = {
+    email : '',
+    password: ''
+  };
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,  
+    private renderer: Renderer2,
+    private router: Router,
+    private customerLoginService: CustomerLoginService,
+    private toastr: ToastrService,
+    private cookieService: CookieService
+  ) {}
 
   
   togglePasswordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
+  }
+
+  //console.log(loginDetails);
+
+  login(data:any):void{
+    console.log("hi");
+    console.log('Login data:', this.loginDetails);
+    if(this.loginDetails.email != '' && this.loginDetails.password != ''){
+      this.customerLoginService.loggingIn(this.loginDetails).subscribe({
+        next: (res: any) => {
+          console.log(res);
+          if (res.loggedIn***REMOVED***1) {
+            this.cookieService.set('jwtToken', res.token);
+            console.log("Logged In")
+            //this.router.navigate(['/dashboard']);   //set the navigation path for customer, after logging in 
+          } else {
+            this.toastr.error(res.message);
+        }
+        },
+        error: (err:any) => {
+        console.log(err)
+        },
+        complete: () => {},
+      });
+    }
+    else{
+      console.log("first");
+      this.toastr.error("Both Email and password are required to login");
+    }
   }
 
 }
