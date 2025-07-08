b63d415633af4e8a3e9b45b6b159c29011147e53
SnehaManoharan1 <snehamanoharan98@gmail.com>
2025-07-08 17:30:39 +0530

    created BE function for getting VZAT Recurring Data from DB with data limits

--- backend/routes/VzatRecurring.js
+++ backend/routes/VzatRecurring.js
@@ -1,8 +1,10 @@
 import express from "express";
-import Post_Vzat_Recurring_Data from "../Controllers/VzatRecurringData.js";
+import Post_Vzat_Recurring_Data from "../Controllers/PostVzatRecurringData.js";
+import Get_Vzat_Recurring_Data from "../Controllers/GetVzatRecurringData.js";
 const router = express.Router();
 
 // Route to /api/vzat_recurring_create_payment_link
 router.route("/").post(Post_Vzat_Recurring_Data);
+router.route("/").get(Get_Vzat_Recurring_Data);
 
 export default router;
