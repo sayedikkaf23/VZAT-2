43b589c23ef01259cb41a40230387f53cb48f2e6
SnehaManoharan1 <snehamanoharan98@gmail.com>
2025-07-10 20:59:07 +0530

    Commom DB Log and retrieving searched VZAT Recurring data modules

--- backend/Controllers/GetSalesForce.js
+++ backend/Controllers/GetSalesForce.js
@@ -1,18 +1,29 @@
 import {connectDB,disconnectDB} from "../config/db.js";
 import SalesForce from "../model/SalesForceModel.js";
-import mongoose from "mongoose";
+import Post_Common_DB_Log_Data from "../Controllers/PostCommonDBLogData.js";
+import _ from 'lodash';
 
 
 const GetSalesForce = async (req, res) => {
-  console.log("hi")
-    if (!req.query) {
+    console.log("hi")
+      const { token } = req.query;
+      if (!token) {
+        const query = {
+          message: "Query is empty"
+        }
+        const data = {
+          message: "Nothing to Process to get the response"
+        }
+        const LogData = Post_Common_DB_Log_Data("/api/salesForce",query,data);
+        console.log(LogData);
         res.statusCode = 404;
-        res.end("Error");
+        res.json(query);
       }
+      else {
 
       console.log(req.query);
 
-      const { token } = req.query;
+    
       console.log(token);
       await connectDB();
     
@@ -21,7 +32,9 @@ const GetSalesForce = async (req, res) => {
         console.log(salesAgent)
         if (salesAgent) {
           //await disconnectDB();
-          return res.json(salesAgent);
+          const LogData = Post_Common_DB_Log_Data("/api/salesForce",req.query, salesAgent);
+          console.log(LogData);
+          res.json(salesAgent);
         }
        // await disconnectDB();
         const data = {
@@ -31,11 +44,20 @@ const GetSalesForce = async (req, res) => {
           mobNo2:0,
           token:0
         }
+        const LogData = Post_Common_DB_Log_Data("/api/salesForce",req.query, data);
+        console.log(LogData);
         res.json(data);
       } catch (err) {
        // await disconnectDB();
+       const data = {
+            message: err
+        }
+        const LogData = Post_Common_DB_Log_Data("/api/salesForce",req.body,data);
+        console.log(LogData);
         throw err;
       }
+
+    }
       
       
 }
