41d7d383916a761d97392ae4f40335f9d7d316c5
SayedIkkaf23 <sayed@yeepeey.com>
2025-07-17 14:18:47 +0400

    changes build

---
+++ frontend/dist/frontend/browser/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  frontendUrl: 'https://vzatnew.yeepeey.com' // Sandbox public URL for payment redirect
+};
