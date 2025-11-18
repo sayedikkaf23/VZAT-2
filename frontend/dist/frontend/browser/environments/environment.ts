30d6bf399e4d4717d46f5e44bade70b6f517835d
mohammedmishalk <mishalnunu@gmail.com>
2025-11-18 19:29:42 +0530

    build

--- frontend/dist/frontend/browser/environments/environment.ts
+++ frontend/dist/frontend/browser/environments/environment.ts
@@ -2,10 +2,10 @@
 export const environment = {
   production: false,
   // For local development, uncomment the line below and comment out the server URL
-  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'http://localhost:3000/api',
   
   // For server deployment or testing against live server
-  // apiUrl: 'https://vzatnew.yeepeey.com/api',
+  apiUrl: 'https://vzatnew.yeepeey.com/api',
   frontendUrl: 'https://vzatnew.yeepeey.com'
     // frontendUrl: 'http://localhost:4200'
 
