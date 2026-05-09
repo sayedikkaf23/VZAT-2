2f95d44656f02c8a71bd84465b428c2165ff96e0
mohammedmishalk <mishalnunu@gmail.com>
2026-05-09 12:08:36 +0530

    build

--- backend/routes/SalesForce.js
+++ backend/routes/SalesForce.js
@@ -3,6 +3,7 @@ import GetSalesForce from "../Controllers/GetSalesForce.js";
 const router = express.Router();
 
 // Route to /api/getSalesForce
+
 router.route("/").get(GetSalesForce);
 
 export default router;
